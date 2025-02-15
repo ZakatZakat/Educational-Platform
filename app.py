9f5acb122bc912c24c4121a4e6517fdcc93abfbf
ZakatZakat <comradefrunze@gmail.com>
2025-02-15 09:42:05 +0300

    fixes

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('student-overview.html')
 
+@app.route('/create_course')
+def create_course():
+    return render_template('create-a-course.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
